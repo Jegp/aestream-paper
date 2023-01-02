159874f35a45e0302c9457fff2c8c1c48a0661e5
Jens E. Pedersen <jensegholm@protonmail.com>
2023-01-02 15:03:58 +0100

    Added reproducibility instructions

---
+++ README.md
@@ -0,0 +1,34 @@
+# AEStream-paper
+
+This repository contains instructions as well as code to reproduce the findings in the paper [AEStream: Accelerated event-based processing with coroutines](https://arxiv.org/abs/2212.10719).
+
+## Installation instructions
+
+We assume [AEStream](https://github.com/norse/aestream) to be installed at > v.0.4.
+Additionally, we require CMake and a C++20 compiler with support for coroutines.
+
+## Reproducing C++ benchmarks
+
+```bash
+$ mkdir build
+$ cd build
+$ cmake -Gninja ..
+$ ./aestream-paper
+```
+
+## Reproducing GPU benchmarks
+
+```bash
+$ python3 src/gpu_benchmark.py
+```
+
+## Reproducing plots
+
+1. Start a [Jupyter Notebook server](https://jupyter.org/)
+2. Execute the `plots.ipynb`
+
+## Acknowledgements
+
+This work was done by [Jens Egholm Pedersen](https://jepedersen.dk) at [KTH](https://kth.se).
+
+We foremost would like to thank Anders Bo Sørensen for his friendly and invaluable help with CUDA and GPU profiling. Emil Jansson deserves our gratitude for scrutinizing and improving the coroutine benchmark C++ code. We gracefully recognize funding from the EC Horizon 2020 Framework Programme under Grant Agreements 785907 and 945539 (HBP). Our thanks also extend to the Pioneer Centre for AI, under the Danish National Research Foundation grant number P1, for hosting us. 
--- event_plot.py
+++ event_plot.py
@@ -2,20 +2,53 @@ import aestream
 import time
 import matplotlib.pyplot as plt
 
+import torch
+import norse
 
 tensors = []
 
-with aestream.FileInput("cam0197.aedat4", (346, 260)) as f:
-    for i in range(50):
-        time.sleep(0.3)
-    for i in range(50):
-        time.sleep(0.1)
-        t = f.read()
-        t = t.bool().float()
-        tensors.append(t)
+with aestream.FileInput("cam0197.aedat4", (346, 260), device="cuda") as f:
+    time.sleep(3)
 
+    # Create horizontal and vertical edge detectors
+    kernel_size = 9
+    gaussian = torch.sigmoid(torch.linspace(-10, 10, kernel_size + 1))
+    kernel = (gaussian.diff() - 0.14).repeat(kernel_size, 1)
+    kernels = torch.stack((kernel, kernel.T))
+    convolution = torch.nn.Conv2d(1, 2, kernel_size, padding=12, bias=False, dilation=3)
+    convolution.weight = torch.nn.Parameter(kernels.unsqueeze(1).to("cuda"))
 
-for i, t in enumerate(tensors):
-    plt.figure(figsize=(6, 4), dpi=300)
-    plt.imshow(t.T, cmap="gray")
-    plt.savefig(f"img/{i}.png", dpi=300)
+    # Create Norse network
+    # - One refractory cell to inhibit pixels
+    # - One convolutional edge-detection layer
+    net = (
+        norse.torch.SequentialState(
+            norse.torch.LIFRefracCell(),
+            convolution,
+        )
+        .eval()
+        .to("cuda")
+    )
+    state = None  # Start with empty state
+
+    frames = []
+    edges = []
+    n = 0
+    with aestream.FileInput(
+        "cam0197.aedat4",
+        (346, 260),
+        device="cuda",
+        ignore_time=False,
+    ) as stream:
+        time.sleep(12)
+        stream.read()
+        with torch.no_grad():
+            with torch.no_grad():
+                while stream.is_streaming() and n < 1000:
+                    time.sleep(0.01)
+                    tensor = stream.read().reshape(1, 1, 346, 260).byte().float()
+                    out, state = net(tensor, state)
+                    frames.append(tensor.cpu())
+                    edges.append(out.cpu())
+                    n += 0
+            torch.save((frames, edges), "data.dat")
