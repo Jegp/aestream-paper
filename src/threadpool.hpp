f8c3c32e14cd48991a7af12bfde48b93966684aa
Jens E. Pedersen <jensegholm@protonmail.com>
2023-07-29 10:43:58 +0200

    Added threadpool header

---
+++ src/threadpool.hpp
@@ -0,0 +1,164 @@
+#ifndef THREADPOOL_HPP
+#define THREADPOOL_HPP
+
+#include <atomic>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <syncstream>
+#include <thread>
+#include <vector>
+
+/// Rudimentary debug printing.
+template <typename... Args> void log(Args &&..._args) {
+  static std::mutex out_mtx;
+
+  std::lock_guard lk{out_mtx};
+  std::array<int, sizeof...(_args)> status{
+      (std::cout << std::forward<Args>(_args), 0)...};
+  std::cout << '\n';
+}
+
+namespace Async {
+using uint = unsigned int;
+using auint = std::atomic<uint>;
+using flag = std::atomic<bool>;
+using lguard = std::lock_guard<std::mutex>;
+template <class T> using uptr = std::unique_ptr<T>;
+
+struct TaskBase {
+  virtual ~TaskBase(){};
+  virtual void operator()() = 0;
+};
+
+using Task = uptr<TaskBase>;
+
+template <typename F, typename Ret, typename... Args>
+struct TaskExecutor : TaskBase {
+  std::function<Ret(Args &&...)> _func;
+  std::tuple<Args &&...> _args;
+
+  constexpr TaskExecutor(F &&func, Args &&...args)
+      : _func(std::forward<F>(func)), _args(std::forward<Args>(args)...) {}
+
+  ~TaskExecutor(){};
+
+  void operator()() override final { std::apply(_func, _args); }
+};
+
+///=============================================================================
+///	Main feature
+///=============================================================================
+
+/// @class Threadpool
+class ThreadPool {
+
+private:
+  /// Task queue.
+  std::vector<Task> buffer;
+
+  /// Mutex for the lock guards.
+  std::mutex mtx;
+
+  // Flag for signalling threads that they should quit.
+  flag halt{false};
+
+  // A container for all the worker threads.
+  std::vector<std::jthread> threads;
+
+  // Worker counter.
+  // Plays a role in the destructor.
+  auint workers{0};
+
+public:
+  ThreadPool(const uint size = std::thread::hardware_concurrency()) {
+    buffer.reserve(1024);
+
+    for (uint i = 0; i < size; ++i) {
+      threads.emplace_back(add_worker());
+    }
+  }
+
+  ~ThreadPool() noexcept {
+    halt.store(true);
+
+    log("Waiting for all workers to finish processing the remaining tasks...");
+
+    while (workers.load() > 0)
+      ;
+  }
+
+  template <typename F, typename... Args>
+  void enqueue(F &&fun, Args &&...args) {
+
+    if (halt.load())
+      return;
+
+    {
+      const lguard lg{mtx};
+      buffer.emplace_back(
+          make_task(std::forward<F>(fun), std::forward<Args>(args)...));
+      //                log("Enqueue | buffer size ", buffer.size());
+    }
+  }
+
+  void stop() {
+    if (halt.load()) {
+      log("Threadpool already stopped.");
+      return;
+    }
+
+    log("Threadpool stopping...");
+
+    {
+
+      lguard lg{mtx};
+
+      // Inform the workers that they should stop.
+      halt.store(true);
+
+      // Empty the queue
+      buffer.clear();
+    }
+  }
+
+private:
+  template <typename Func, typename... Args>
+  Task make_task(Func &&fun, Args &&...args) {
+    using Ret = typename std::invoke_result<Func, Args...>::type;
+    return std::make_unique<TaskExecutor<Func, Ret, Args...>>(
+        std::forward<Func>(fun), std::forward<Args>(args)...);
+  }
+
+  std::jthread add_worker() {
+    return std::jthread([&]() mutable {
+      ++workers;
+
+      std::vector<Task> _buffer;
+
+      _buffer.reserve(1024);
+
+      while (!(halt.load() && buffer.empty())) {
+
+        {
+          const lguard lg(mtx);
+          //                            log("Swapping | _buffer size: ",
+          //                            _buffer.size(), " | buffer size: ",
+          //                            buffer.size());
+          _buffer.swap(buffer);
+        }
+
+        for (uint i = 0; i < _buffer.size(); ++i) {
+          // Execute the task
+          (*_buffer[i])();
+        }
+        _buffer.clear();
+        // log("Finished | buffer size: ", _buffer.size());
+      }
+
+      --workers;
+    });
+  }
+};
+} // namespace Async
+#endif // THREADPOOL_HPP
--- src/threads.cpp
+++ src/threads.cpp
@@ -1,5 +1,5 @@
-template<class Task>
-void ThreadState<Task>::consumer() {
+template <class Task> void ThreadState<Task>::consumer() {
+  unsigned long sum = 0;
   while (true) {
     auto opt = event_queue.pop();
     if (opt.has_value()) {
@@ -7,22 +7,21 @@ void ThreadState<Task>::consumer() {
       for (const auto &event : *opt.value()) {
         sum += Task::apply(event.x, event.y);
       }
-      sum_value.fetch_add(sum);
     } else {
-      return;
+      break;
     }
   }
+  sum_value.fetch_add(sum);
 }
 
-template<class Task>
+template <class Task>
 EventPtr ThreadState<Task>::reserve_buffer(size_t buffer_size) {
   auto buffer = EventPtr{new EventVec};
   buffer->reserve(buffer_size);
   return buffer;
 }
 
-template<class Task>
-void ThreadState<Task>::producer() {
+template <class Task> void ThreadState<Task>::producer() {
   EventPtr buffer = reserve_buffer(buffer_size);
   for (const auto &event : events) {
     buffer->push_back(event);
@@ -38,17 +37,16 @@ void ThreadState<Task>::producer() {
   event_queue.shutdown();
 }
 
-template<class Task>
-ThreadState<Task>::ThreadState(const EventVec &ev,
-                         size_t buf_size, size_t n_consumers)
+template <class Task>
+ThreadState<Task>::ThreadState(const EventVec &ev, size_t buf_size,
+                               size_t n_consumers)
     : buffer_size{buf_size}, events{ev}, sum_value{0} {
   for (size_t i = 0; i < n_consumers; i++) {
     consumer_threads.emplace_back(std::thread{&ThreadState::consumer, this});
   }
 }
 
-template<class Task>
-size_t ThreadState<Task>::run() {
+template <class Task> size_t ThreadState<Task>::run() {
   auto p_thread = std::thread(&ThreadState::producer, this);
 
   p_thread.join();
